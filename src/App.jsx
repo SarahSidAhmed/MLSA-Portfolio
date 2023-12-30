0e626b524b9479a4624aea2ea32361830e859a64
Sarah Sid <109920075+SarahSidAhmed@users.noreply.github.com>
2023-12-30 19:20:05 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Sid Ahmed Sarah",
+  title: "Power BI Anlysis & Android Developper",
+  email: "ms_sidahmed@esi.dz",
+  gitHub: "https://github.com/SarahSidAhmed",
+  instagram: "https://instagram.com/cs_siddev?igshid=MmVlMjlkMTBhMg==",
+  linkedIn: "https://www.linkedin.com/in/sarah-sidwebdev/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
